4a2a67224ac748f3d3d2ff7464e47dcc68cd1f8c
Dongmin Lee <ldm2468@snu.ac.kr>
2024-11-21 12:31:24 -0500

    Implement caching of file list query results

--- src/file_server.py
+++ src/file_server.py
@@ -1,8 +1,17 @@
+file_list_cache = {}
+
 def send(name: str, filename: str):
     print(f'Sending {filename} to {name}...')
+    # TODO: implement send
 
 def receive(name: str, filename: str):
-    pass
+    print(f'Receiving {filename} from {name}...')
+    # TODO: implement receive
 
 def list(name: str):
-    pass
+    global file_list_cache
+    print(f'Retrieving list of files stored by {name}...')
+    # TODO: populate l with retrieved file list
+    l = {'todo': 53600} # size in bytes
+    file_list_cache[name] = l
+    return l
