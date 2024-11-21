f1a796f6f773c0610cf1e4899833c2ebca82e9a6
Dongmin Lee <ldm2468@snu.ac.kr>
2024-11-21 10:17:04 -0500

    Make list_server.register() fetch own public ip
    Note that the method used to fetch the public ip is far from foolproof and can be exploited.

--- src/list_server.py
+++ src/list_server.py
@@ -3,9 +3,11 @@ import base64
 import requests
 from config import LIST_SERVER
 
+my_public_ip = None
+
 def register(name: str, port: int, pubkey: bytes):
     """
-    Send a register request to the list server.
+    Send a register request to the list server. If successful, also sets my_public_ip.
     Args:
         name: Unique identifier for this user.
         port: Port number that this node listens on.
@@ -14,12 +16,19 @@ def register(name: str, port: int, pubkey: bytes):
     Returns:
         True if registration was successful.
     """
+    global my_public_ip
     r = requests.post(f'{LIST_SERVER.ADDRESS}/api/register', data={
         'name': name,
         'port': port,
         'pubkey': base64.b64encode(pubkey)
     })
-    return r.text == 'OK'
+    if r.text != 'OK':
+        return False
+    list = list_nodes()
+    if name not in list:
+        return False
+    my_public_ip = list[name]['ip']
+    return True
 
 def list_nodes():
     """
