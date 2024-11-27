46eac09233275eace7d6f66f8ce1068f36a177c4
Dongmin Lee <ldm2468@gmail.com>
2024-11-27 03:37:08 -0500

    Implement generate_cycle()

--- src/crypto.py
+++ src/crypto.py
@@ -1,3 +1,4 @@
+import random
 import struct
 
 
@@ -33,3 +34,29 @@ def unpad_payload(payload: bytes) -> bytes:
     if 4 + size > len(payload):
         raise ValueError("malformed payload")
     return payload[4:4+size]
+
+def generate_cycle(users: dict[str, dict], orig: str, dest: str, length: int) -> list[tuple[str, dict]]:
+    """
+    Generate a cycle that starts from orig, passes through dest, and returns back to orig.
+    Args:
+        users: Dict mapping names to users (returned from list_server.list_nodes()).
+        orig: Origin name.
+        dest: Destination name.
+        length: Length of the cycle.
+
+    Returns:
+        A list of tuples in the cycle.
+    """
+    if length < 6:
+        raise ValueError("cycle length must be at least 6")
+    # convert to a list of tuples while excluding the orig and dest
+    users_excluding_endpoints = [(k, v) for k, v in users.items() if k not in [orig, dest]]
+    cycle = random.sample(users_excluding_endpoints, length - 2) # does not include orig and dest yet
+
+    # choose where to insert destination (should not be too close to origin)
+    dest_loc = random.randint(2, len(cycle) - 2)
+    cycle.insert(dest_loc, (dest, users[dest]))
+    cycle.append((orig, users[orig]))
+
+    return cycle
+
